13e23b641154548ddafd6b9ae59c87a371944fbb
Margo <margo@margonline.co.uk>
2013-08-22 17:24:26 +0100

    new msg at start of new set

--- tennis.rb
+++ tennis.rb
@@ -1,9 +1,3 @@
-# v.2 - incorporates refactoring suggestions and naming conventions suggested by Evegeny, 21 Aug 2013:
-# 			changed the score array to a constant in Match class
-#				to keep track of the player who won the point, used a reference to one of the player instances
-# v.1 - incorporates game and set scoring as well as point scoring
-# 2 classes - Player holds play info and scores; Match contains methods to handle the scoring
-
 class Player
 	attr_accessor :points, :games, :sets
 	def initialize(fname,lname)
@@ -13,15 +7,15 @@ class Player
 		@fname = fname
 		@lname = lname
 	end
-	
+
 	def fullname
 		@fname + ' ' + @lname
 	end
-	
+
 	def score
 		'Sets:' + @sets.to_s + ' Games:' + @games.to_s + ' Points:' + @points.to_s
 	end
-	
+
 	def games_sets
 		'Sets:' + @sets.to_s + ' Games:' + @games.to_s
 	end
@@ -29,7 +23,7 @@ end
 
 class Match
 	attr_accessor :game_over, :set_over, :match_over, :point_winning_player, :server
-	
+
   SCORE = %w(0 15 30 40 advantage game)
 
 	def initialize(server)
@@ -38,7 +32,7 @@ class Match
 		puts "Let the match begin!"
 		puts "#{@server.fullname} to serve first."
 	end
-	
+
 	def play(player1, player2)
 		display_points(player1, player2)
 		until match_over?(player1, player2)
@@ -50,12 +44,12 @@ class Match
 	end
 
 private
-def play_point(player1, player2)
+	def play_point(player1, player2)
 		prompt(player1, player2)
 		point_winner = gets.chomp
     @point_winning_player = point_winner == '1' ? player1 : player2
 	end
-	
+
 	def change_score(player1,player2,point_winning_player)
 		if advantage?(player1,player2) 
 			is_advantage(player1,player2,@point_winning_player) 
@@ -64,24 +58,24 @@ def play_point(player1, player2)
 		else
 			increment_score(@point_winning_player)	
 		end	
-	
+
 		if game?(player1, player2)
 			game_over(player1,player2,@point_winning_player)
 		end
 	end
-		
+
 	def advantage?(player1,player2)
 		player1.points == 4 || player2.points == 4 
 	end
-	
+
 	def is_30_40?(player1,player2)
 		(player1.points == 3 || player2.points == 3) && (player1.points - player2.points).abs == 1
 	end
-	
+
 	def break_point?(player1,player2)
-		
+
 	end
-	
+
 	def increment_score(point_winning_player)
   		@point_winning_player.points += 1
 	end
@@ -89,6 +83,7 @@ def play_point(player1, player2)
 	def deuce?(player1,player2)
 		player1.points == 4 && player2.points == 4
 	end
+
 	def game?(player1, player2)
 		if (player1.points == 4 || player2.points == 4)
 			(player1.points - player2.points).abs >= 2 ? true : false
@@ -107,7 +102,7 @@ def play_point(player1, player2)
 			@point_winning_player.points += 1
 		end
 	end
-	
+
 	def is_advantage(player1,player2,point_winning_player)
     point_losing_player = player1 == @point_winning_player ? player2 : player1
     if @point_winning_player.points > point_losing_player.points
@@ -153,7 +148,8 @@ def play_point(player1, player2)
 	def tiebreak?(player1, player2)
 		 player1.games == 6 && player2.games == 6 
 	end
-	
+
+	def play_tiebreak(player1, player2, point_winning_player)
 		until (player1.points >= 7 || player2.points >= 7) && ((player1.points - player2.points).abs >= 2)
 			play_point(player1, player2)
 			increment_score(@point_winning_player)
@@ -163,30 +159,30 @@ def play_point(player1, player2)
 		@game_over = true
 		display_score(player1,player2)
 	end
-	
+
 	def set?(player1, player2)
 		((player1.games == 6 || player2.games == 6) && (player1.games - player2.games).abs >= 2) || (player1.games == 7 || player2.games == 7) 
 	end
-	
+
 	def match_over?(player1, player2)
 		player1.sets == 2 || player2.sets == 2 
 	end
-	
+
 	def match_over(player1, player2,point_winning_player)
 		puts "*************************************************"
 		puts "GAME, SET AND MATCH: #{@point_winning_player.fullname}!"
 		puts "*************************************************"
 		display_score(player1, player2)
 	end
-	
+
 	def set_point_winner(point_winning_player)
     	point_winner = @point_winning_player = point_winner == 1 ? player1 : player2
 	end 
-	
+
 	def prompt(player1, player2)
 		print "Who won the point? #{player1.fullname} (1) or #{player2.fullname} (2)\n> "
 	end
-	
+
   def set_game_flags
     @game_over = @set_over = false
   end
@@ -201,12 +197,12 @@ def play_point(player1, player2)
 			display_points(player1, player2)
 		end
 	end
-	
+
 	def display_points(player1, player2)
 		puts "#{player1.fullname}: #{SCORE[player1.points]}"
 		puts "#{player2.fullname}: #{SCORE[player2.points]}"
 	end
-	
+
 	def display_tiebreak_score(player1, player2)
 		puts "#{player1.fullname}: #{player1.points}"
 		puts "#{player2.fullname}: #{player2.points}"
@@ -214,7 +210,7 @@ def play_point(player1, player2)
 
 	def intro_new_set
 		puts '------------------------------'
-		puts 'NEW SET: to serve'
+		puts "NEW SET: #{@server.fullname} to serve"
 		puts '------------------------------'
 	end
 
@@ -223,8 +219,8 @@ def play_point(player1, player2)
 		puts 'A tiebreak will now be played.'
 		puts '------------------------------'
 	end
-
 end
+
 
 puts "MARGO'S TENNIS SCORING"
 print "Player 1 first name:  "
